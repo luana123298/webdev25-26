d8334f44a005710427e0c3f9fa5e722912e20c5e
luana123298 <Luanad@nycstudents.net>
2026-01-06 19:08:46 +0000

    yyy

---
+++ WDLesson3_3Practical/script.js
@@ -0,0 +1,4 @@
+/* Challenge 2: Create a function for each image that display a description of the picture into the div with an id of highlight*/
+function rose(){
+    document.getElementById("highlight").src="data.image/https://cdn.shopify.com/s/files/1/0522/0021/0585/files/Rose.jpg?v=1743064665"
+}
