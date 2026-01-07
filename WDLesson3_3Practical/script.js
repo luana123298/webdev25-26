6a47bddd58d559b7c2e9ca5734ce801eed5e16d3
luana123298 <Luanad@nycstudents.net>
2026-01-07 19:08:07 +0000

    ooo

--- WDLesson3_3Practical/script.js
+++ WDLesson3_3Practical/script.js
@@ -1,4 +1,18 @@
 /* Challenge 2: Create a function for each image that display a description of the picture into the div with an id of highlight*/
 function rose(){
-    document.getElementById("highlight").src="data.image/https://cdn.shopify.com/s/files/1/0522/0021/0585/files/Rose.jpg?v=1743064665"
+    document.getElementById("highlight").innerHTML="this is a rose";
+}
+
+function daisy(){
+     document.getElementById("highlight").innerHTML="this is a daisy";
+}
+
+function lavinder(){
+     document.getElementById("highlight").innerHTML="this is a lavinder";
+}
+function blooming(){
+     document.getElementById("highlight").innerHTML="this is a blooming";
+}
+function blue (){
+     document.getElementById("highlight").innerHTML="this is a blue";
 }
